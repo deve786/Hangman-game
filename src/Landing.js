7d53fb232e1b6684955297c41c82a1e42aa943cb
Developer <ijasahmediku987@gmail.com>
2024-05-05 16:09:05 +0530

    lInkFix

--- src/Landing.js
+++ src/Landing.js
@@ -9,7 +9,7 @@ function Landing() {
         <h2 className='pt-3 text-center fw-bold'>Hangman</h2>
         <div className='d-flex align-items-center justify-content-center'><p className='pt-3 container-fluid para text-center'>The Hangman game is a classic word-guessing game that has been popular for generations.  It combines elements of vocabulary challenge and strategy with simple rules, making it accessible and enjoyable for people of all ages.</p></div>
         <div className='d-flex pt-3 align-items-center justify-content-center '>
-          <button className='btns'><Link to={'/game'} style={{textDecoration:'none',color:'black'}}>Play</Link></button>
+        <Link to={'/game'} style={{textDecoration:'none',color:'black'}}> <button className='btns'>Play</button></Link>
         </div>
 
       </div>
